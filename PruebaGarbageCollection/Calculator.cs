ffed87ea634af1ab579a34263d66fe5982f71862
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-09-05 22:55:37 -0600

    Empezando a leer: Thread safety and the Dispose method

---
+++ PruebaGarbageCollection/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGarbageCollection
+{
+    class Calculator : IDisposable
+    {
+        private bool disposed = false;
+
+        public Calculator()
+        {
+            Console.WriteLine("Calculator being created");
+        }
+
+        ~Calculator()
+        {
+            Console.WriteLine("Calculator being finalized");
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (!disposed)
+                Console.WriteLine("Calculator being disposed");
+
+            disposed = true;
+            GC.SuppressFinalize(this);
+        }
+
+        public int Divide(int first, int second)
+        {
+            return first / second;
+        }
+    }
+}
--- PruebaGarbageCollection/Program.cs
+++ PruebaGarbageCollection/Program.cs
@@ -10,6 +10,9 @@ namespace PruebaGarbageCollection
     {
         static void Main(string[] args)
         {
+            using (Calculator calculator = new Calculator())
+                Console.WriteLine($"120 / 15 = {calculator.Divide(120, 15)}");
+            Console.WriteLine("Program finishing");
         }
     }
 }
